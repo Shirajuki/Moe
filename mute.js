c483cde2c9a2b6801b3151b1442296e6bbdcd2af
Shirajuki <vulturegfd@reborn.com>
2017-08-08 19:46:32 +0200

    4

--- mute.js
+++ mute.js
@@ -1,9 +1,5 @@
 var video = document.getElementById('bgvid');
 var muteButton = document.getElementById("mute");
-var cont = document.getElementById("contact");
-
-cont.innerHTML = cont.innerHTML.replace('#cocks', 'mailto:ppe@tuuba.moe');
-
 
 muteButton.addEventListener("click", function() {
 	if (video.muted == false) {
