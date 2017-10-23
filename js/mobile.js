dd7167c51664f4581a3f0e320af2c94c8b9fb5ec
Shirajuki <vulturegfd@reborn.com>
2017-10-23 21:31:04 +0200

    asd

---
+++ js/mobile.js
@@ -0,0 +1,11 @@
+// in case of a viewport
+if (document.documentElement.clientWidth < 900) {
+// scripts --> redirect
+    window.location = "./MOBILE/index.html"
+}
+
+// in case of a screen
+if (screen.width < 900) {
+// scripts --> redirect
+    window.location = "./MOBILE/index.html"
+}
